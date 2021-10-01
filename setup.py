7d375cb5a88cb0d3d4696290c66a2ce07eec2370
raki dedigama <raki.dedigama@solteq.com>
2021-10-01 12:53:27 +0300

    feat : setup file for package : trba

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+
+    name ='TRBA', 
+    url='https://github.com/raki-dedigama', 
+    author = 'Raki Dedigama', 
+    author_email = 'rakkitha.da@gmail.com', 
+    packages = ['trba'], 
+    install_requires = ['numpy'], 
+    version = '0.1',
+    license = 'MIT', 
+    description = "A package for TRBA model for OCR",
+)
